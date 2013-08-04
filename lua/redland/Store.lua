a4010053d9b17bf6248e9cc791506d7e91cc2ad5
Georgi Rybakov <georgi.rybakov@gmx.net>
2013-08-04 21:47:39 +0200

    Parameter optional gemacht

--- lua/redland/Store.lua
+++ lua/redland/Store.lua
@@ -128,12 +128,17 @@ function Store.lookup(world, idx)
    end
 end
 
-function Store:new(world, hash, storage_name, name)
+function Store:new(world, storage_name, name, params)
+   local bindings_hash
+   if params.hash
+   then
+      bindings_hash =  params.hash:get_bindings_hash()
+   end
    local bindings_store =  bindings_redland_module.store.new(
          world:get_bindings_world()
-      ,  hash:get_bindings_hash()
       ,  storage_name:get_content()
-      ,  name:get_content() )
+      ,  name:get_content()
+      ,  { hash =  bindings_hash })
    if bindings_store
    then
       self:bindings_store_factory(bindings_store)
