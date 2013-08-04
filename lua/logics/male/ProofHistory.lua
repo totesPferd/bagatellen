1a2064846c993f3c9552cfb71d31d978d4446996
Georgi Rybakov <georgi.rybakov@gmx.net>
2013-08-04 20:47:10 +0200

    logics.male.Proof

--- lua/logics/male/ProofHistory.lua
+++ lua/logics/male/ProofHistory.lua
@@ -4,24 +4,27 @@ local ProofHistory =  Type:__new()
 
 
 package.loaded["logics.male.ProofHistory"] =  ProofHistory
-local Dict =  require "base.type.Dict"
 local Indentation =  require "base.Indentation"
 local Set =  require "base.type.Set"
 local String =  require "base.type.String"
 
-function ProofHistory:new()
+function ProofHistory:new(proof)
    local retval =  ProofHistory:__new()
-   retval.history =  Dict:empty_dict_factory()
+   retval.proof =  proof
    retval.proven_goals =  Set:empty_set_factory()
    return retval
 end
 
+function ProofHistory:get_proof()
+   return self.proof
+end
+
 function ProofHistory:deref(goal)
-   return self.history:deref(goal)
+   return self:get_proof():deref(goal)
 end
 
 function ProofHistory:add(goal, rule)
-   self.history:add(goal, rule)
+   self:get_proof():add(goal, rule)
 end
 
 function ProofHistory:is_proven(goal)
@@ -33,9 +36,9 @@ function ProofHistory:mark_as_proven(goal)
 end
 
 function ProofHistory:drop_all_assumes()
-   for goal in self.history:keys()
-   do local rule =  self.history:deref(goal)
-      self.history:drop(goal)
+   self:get_proof():drop_all_assumes()
+   for goal in self:get_proof():keys()
+   do local rule =  self:get_proof():deref(goal)
       self.proven_goals:drop(goal)
    end
 end
@@ -45,16 +48,16 @@ function ProofHistory:mark_all_as_unproven()
 end
 
 function ProofHistory:tell_proven_goals(other)
-   for goal in self.history:get_keys()
-   do local rule =  self.history:deref(goal)
-      other:add(goal, rule)
+   self:get_proof():tell_proven_goals(other:get_proof())
+   for goal in self:get_proof():get_keys()
+   do local rule =  self:get_proof():deref(goal)
       other:mark_as_proven(goal)
    end
 end
 
 function ProofHistory:__diagnose_single_line(indentation)
    indentation:insert(String:string_factory("(logics.male.ProofHistory "))
-   self.history:__diagnose_single_line(indentation)
+   self:get_proof():__diagnose_single_line(indentation)
    indentation:insert(String:string_factory(" "))
    self.proven_goals:__diagnose_single_line(indentation)
    indentation:insert(String:string_factory(")"))
@@ -68,7 +71,7 @@ function ProofHistory:__diagnose_multiple_line(indentation)
    local deeper_indentation =
       indentation:get_deeper_indentation_factory {}
    is_last_elem_multiple_line =
-      self.history:__diagnose_complex(deeper_indentation)
+      self:get_proof():__diagnose_complex(deeper_indentation)
    deeper_indentation:insert_newline()
    is_last_elem_multiple_line =
       self.proven_goals:__diagnose_complex(deeper_indentation)
