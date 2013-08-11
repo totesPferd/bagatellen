b6e39ebe640b5b7a5d2f7244f1aa54d02b9350f4
Georgi Rybakov <georgi.rybakov@gmx.net>
2013-08-11 04:37:50 +0200

    bindings.redland.formatter

---
+++ c/lua/bindings/redland/formatter.c
@@ -0,0 +1,56 @@
+#include "formatter.h"
+#include "defs.h"
+#include <lauxlib.h>
+
+int
+lua_bindings_redland_formatter_gc(lua_State *L) {
+   librdf_query_results_formatter **pp_formatter
+      =  (librdf_query_results_formatter **) luaL_checkudata(
+            L
+         ,  -1
+         ,  qfmt_userdata_type );
+
+   lua_pop(L, 1);
+
+   librdf_free_query_results_formatter(*pp_formatter);
+
+   return 0;
+}
+
+
+/* ------------------------------------------------------------ */
+
+int
+lua_bindings_redland_formatter_new_mt(lua_State *L) {
+   luaL_newmetatable(L, qfmt_userdata_type);
+
+   lua_pushcfunction(L, &lua_bindings_redland_formatter_gc);
+   lua_setfield(L, -2, "__gc");
+
+   return 1;
+}
+
+int
+lua_bindings_redland_formatter_wrap(lua_State *L, librdf_query_results_formatter *p_formatter) {
+   if (p_formatter) {
+      librdf_query_results_formatter **pp_formatter
+         =  (librdf_query_results_formatter **) lua_newuserdata(
+               L
+            ,  sizeof(void *) );
+      *pp_formatter =  p_formatter;
+
+      lua_insert(L, -2);
+      lua_setmetatable(L, -2);
+   
+      return 1;
+   } else {
+      return 0;
+   }
+}
+
+int
+luaopen_bindings_redland_formatter(lua_State *L) {
+   lua_newtable(L);
+
+   return 1;
+}
---
+++ c/lua/bindings/redland/formatter.h
@@ -0,0 +1,21 @@
+#ifndef LUA_BINDINGS_REDLAND_FORMATTER_H
+#define LUA_BINDINGS_REDLAND_FORMATTER_H
+#include <librdf.h>
+#include <lua.h>
+
+int
+lua_bindings_redland_formatter_gc(lua_State *);
+
+int
+lua_bindings_redland_formatter_new(lua_State *);
+
+int
+lua_bindings_redland_formatter_new_mt(lua_State *);
+
+int
+lua_bindings_redland_formatter_wrap(lua_State *, librdf_query_results_formatter *);
+
+int
+luaopen_bindings_redland_formatter(lua_State *);
+
+#endif
