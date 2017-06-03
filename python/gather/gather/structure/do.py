232d1c198e517f567d42cbb99b2bde2231b967b6
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-06-03 16:39:28 +0200

    ...

--- python/gather/gather/structure/do.py
+++ python/gather/gather/structure/do.py
@@ -13,15 +13,15 @@ def get(registry, url):
          line =  d.bozo_exception.getLineNumber()
          msg =  d.bozo_exception.getMessage()
          bozo =  gather.structure.bozo.Bozo(col, line, msg)
-      entity =  gather.structure.entity.Entity(url, d.etag, bozo)
-      registry.store_entity(entity)
+      retval =  gather.structure.entity.Entity(url, d.etag, bozo)
+      registry.store_entity(retval)
       if d.feed.links:
          for link in d.feed.links:
             if link.rel == "alternate":
                href = get(registry, link.href)
                if link.type == "application/atom+xml":
-                  entity.add_feed("Atom", href)
+                  retval.add_feed("Atom", href)
                elif link.type == "application/rss+xml":
-                  entity.add_feed("RSS", href)
+                  retval.add_feed("RSS", href)
    return retval
    
