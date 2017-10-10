9172044efe750769185dcca50cc87117711b0650
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-10-10 17:17:32 +0200

    diagnose f. logics::pel::Literal

--- lua/logics/pel/Literal.lua
+++ lua/logics/pel/Literal.lua
@@ -54,4 +54,31 @@ function Literal:get_unifier(other)
    end
 end
 
+function Literal:__diagnose_single_line(indentation)
+   local p_name =  self:get_pred():get_name()
+   indentation:insert(String:string_factory("(logics::pel::Literal "))
+   indentation:insert(p_name)
+   for term in self.term_list:elems()
+   do indentation:insert(String:string_factory(" "))
+      term:__diagnose_single_line(indentation)
+   end
+   indentation:insert(String:string_factory(")"))
+end
+
+function Literal:__diagnose_multiple_line(indentation)
+   local p_name =  self:get_pred():get_name()
+   indentation:insert(String:string_factory("(logics::pel::Literal "))
+   indentation:insert(p_name)
+   local is_last_elem_multiple_line =  true
+   local deeper_indentation =
+      indentation:get_deeper_indentation_factory {}
+   for term in self.term_list:elems()
+   do deeper_indentation:insert_newline()
+      is_last_elem_multiple_line =
+         term:__diagnose_complex(deeper_indentation)
+   end
+   deeper_indentation:save()
+   indentation:insert(String:parenthesis_off_depending_factory(is_last_elem_multiple_line))
+end
+
 return Literal
