f0e068953453e0b819cf60a4bdca8cf081ed2acc
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-10-10 06:14:46 +0200

    diagnose f. logics::pel::Variable ergänzt

--- lua/logics/pel/Variable.lua
+++ lua/logics/pel/Variable.lua
@@ -3,6 +3,7 @@ local Type =  require "base.type.aux.Type"
 local Variable =  Type:__new()
 
 package.loaded["logics.pel.Variable"] =  Variable
+local String =  require "base.type.String"
 
 function Variable:new(sort)
    local retval =  self:__new()
@@ -20,6 +21,30 @@ end
 
 function Variable:set_name(name)
    self.name =  name
+end
+
+function Variable:get_non_nil_name()
+   return self.name or String:string_factory("?")
+end
+
+function Variable:__diagnose_single_line(indentation)
+   indentation:insert(String:string_factory("(logics::pel::Variable "))
+   indentation:insert(self:get_non_nil_name())
+   indentation:insert(String:string_factory(": "))
+   indentation:insert(self:get_sort():get_name())
+   indentation:insert(String:string_factory(")"))
+end
+
+function Variable:__diagnose_multiple_line(indentation)
+   indentation:insert(String:string_factory("(logics::pel::Variable"))
+   indentation:insert_newline()
+   local deeper_indentation =
+      indentation:get_deeper_indentation_factory {}
+   deeper_indentation:insert(self:get_non_nil_name())
+   deeper_indentation:insert(String:string_factory(": "))
+   deeper_indentation:insert(self:get_sort():get_name())
+   deeper_indentation:save()
+   indentation:insert(String:string_factory(" )"))
 end
 
 return Variable
