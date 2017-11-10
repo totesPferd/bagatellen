b1c7516091e9ad56ffc03d17e59b9f7f7a771db7
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-11-10 21:43:29 +0100

    letzten commit k

--- lua/logics/male/VarAssgnm.lua
+++ lua/logics/male/VarAssgnm.lua
@@ -3,9 +3,20 @@ local Type =  require "base.type.aux.Type"
 local VarAssgnm =  Type:__new()
 
 package.loaded["logics.male.VarAssgnm"] =  VarAssgnm
+local Dict =  require "base.type.Dict"
 
 function VarAssgnm:new()
-   return self:__new()
+   local retval =  self:__new()
+   retval.dict =  Dict:empty_dict_factory()
+   return retval
+end
+
+function VarAssgnm:deref(var)
+   return self.dict:deref(var)
+end
+
+function VarAssgnm:add(var, val)
+   self.dict:add(var, val)
 end
 
 return VarAssgnm
---
+++ lua/logics/male/Variable.lua
@@ -0,0 +1,56 @@
+local Type =  require "base.type.aux.Type"
+
+local Variable =  Type:__new()
+
+package.loaded["logics.male.Variable"] =  Variable
+
+function Variable:new()
+   return self:__new()
+end
+
+function Variable:get_variable()
+   return self
+end
+
+function Variable:get_val()
+   return self.val
+end
+
+function Variable:set_val(val)
+   self.val =  val
+end
+
+function Variable:equate(val)
+   local this_val =  self:get_val()
+   if this_val
+   then
+      return this_val:equate(val)
+   elseif self == val
+   then
+      return true
+   else
+      self:set_val(val)
+      return true
+   end
+end
+
+function Variable:devar(var_assgnm)
+   local val =  var_assgnm:deref(self)
+   if val
+   then
+      return val
+   else
+      val =  self:get_val()
+      local new_var
+      if val
+      then
+         local new_var =  val:devar(var_assgnm)
+      else
+         local new_var =  self:new()
+      end
+      var_assgnm:add(self, new_var)
+      return new_var
+   end
+end
+
+return Variable
