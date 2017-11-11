463a9d2bfcb0c81381870bc482226659f899c586
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-11-11 17:24:19 +0100

    Reste von frueher geloescht

--- lua/logics/o01/pel/symbol/Interface.lua
+++
@@ -1,9 +0,0 @@
-local Type =  require "base.type.aux.Type"
-
-local Interface =  Type:__new()
-
-package.loaded["logics.place.simple.symbol.Interface"] =  Interface
-
-function Interface:new()
-   return self:__new()
-end
