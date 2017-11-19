7203e8233a5b5b1f43e53d9196fba57fb32caaa3
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-11-19 15:03:19 +0100

    ...

--- lua/logics/qualifier/MetaVariable.lua
+++ lua/logics/qualifier/MetaVariable.lua
@@ -6,14 +6,13 @@ package.loaded["logics.qualifier.MetaVariable"] =  MetaVariable
 local CompoundQualifier =  require "logics.qualifier.CompoundQualifier"
 local String =  require "base.type.String"
 
-function MetaVariable:new(rhs_object)
+function MetaVariable:new()
    local retval =  MALEMetaVariable.new(self)
-   retval.rhs_object =  rhs_object
    return retval
 end
 
 function MetaVariable:copy()
-   return self.__index:new(self:get_rhs_object())
+   return self.__index:new()
 end
 
 function MetaVariable:new_compound_qualifier(terminal, qualifier)
@@ -21,10 +20,6 @@ function MetaVariable:new_compound_qualifier(terminal, qualifier)
 end
 
 function MetaVariable:get_compound_qualifier_cast()
-end
-
-function MetaVariable:get_rhs_object()
-   return self.rhs_object
 end
 
 function MetaVariable:destruct_terminal(terminal)
@@ -38,24 +33,11 @@ function MetaVariable:destruct_terminal(terminal)
       end
    end
 
-   local rhs_obj_compound_qualifier_cast
-      =  self:get_rhs_object():get_compound_qualifier_cast()
-   if
-         rhs_obj_compound_qualifier_cast
-     and rhs_obj_compound_qualifier_cast:get_terminal() == terminal
-   then
-      self:set_val(rhs_obj_compound_qualifier_cast)
-      return rhs_obj_compound_qualifier_cast:get_qualifier()
-   end
-
    local new_var =  self:copy()
    local new_val =  self:new_compound_qualifier(terminal, new_var)
    self:set_val(new_val)
    return new_var
-end
 
-function MetaVariable:append_qualifier(qualifier)
-   self:get_rhs_object():append_qualifier(qualifier)
 end
 
 function MetaVariable:is_id()
@@ -63,8 +45,6 @@ function MetaVariable:is_id()
    if this_val
    then
       return this_val:is_id()
-   else
-      return self:get_rhs_object():is_id()
    end
 end
 
