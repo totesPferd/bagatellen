3e538548ff93b2c882586915aacaf7bad6337462
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-11-21 17:44:05 +0100

    ...

--- lua/logics/ql/Rules.lua
+++ lua/logics/ql/Rules.lua
@@ -4,6 +4,7 @@ local ObjectVariable =  require "logics.ql.ObjectVariable"
 local Resolve =  require "logics.male.rule.Resolve"
 local Set =  require "base.type.Set"
 local ToLiteral =  require "logics.ql.ToLiteral"
+local VarAssgnm =  require "logics.male.VarAssgnm"
 
 local Rules =  require "logics.male.Rules"
 
@@ -47,6 +48,19 @@ function Rules:gen_td()
    local premises =  Set:empty_set_factory()
    premises:add(lhs_cath)
    premises:add(hypoth)
+
+   local clause =  Clause:new(premises, conclusion)
+   return Resolve:new(clause)
+end
+
+function Rules:gen_trans_literal(to_literal)
+   local var_assgnm =  VarAssgnm:new()
+   local devared_literal =  to_literal:devar(var_assgnm)
+   local rhs_var =  ObjectVariable:new()
+   local premis =  ToLiteral(to_literal:get_rhs_term(), rhs_var)
+   local conclusion =  ToLiteral(to_literal:get_lhs_term(), rhs_var)
+   local premises =  Set:empty_set_factory()
+   premises:add(premis)
 
    local clause =  Clause:new(premises, conclusion)
    return Resolve:new(clause)
