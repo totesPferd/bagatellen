ff9ee6de801871f720c50c7bae5ab61ade764042
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-11-22 11:48:34 +0100

    ...

--- lua/logics/ql/ProofState.lua
+++ lua/logics/ql/ProofState.lua
@@ -1,10 +1,11 @@
 local ProofState =  require "logics.male.ProofState"
-local Rules =  require "logics.ql.Rules"
+local ReflRule =  require "logics.ql.rule.Refl"
+local TransRule =  require "logics.ql.rule.Trans"
 
 function ProofState:apply_assumptions()
    local retval =  true
    for conclusion in self:get_conclusions():elems()
-   do local rule =  Rules:gen_refl()
+   do local rule =  ReflRule:new()
       retval =  self:apply_rule(rule, conclusion)
       if not retval
       then
@@ -12,6 +13,27 @@ function ProofState:apply_assumptions()
       end
    end
    return retval
+end
+
+function ProofState:apply_lhs_literal_tactics(proof)
+   local rep =  true
+   while rep
+   do rep =  false
+      for conclusion in self:get_conclusions():elems()
+      do local rule =  TransRule:new()
+         local success =  self:apply_rule(rule, conclusion)
+         if success
+         then
+            local lhs_literal =  rule:get_lhs_literal()
+            local lhs_literal_copy =  proof:search_simply(lhs_literal)
+            if lhs_literal_copy
+            then
+               rep =  true
+               break
+            end
+         end
+      end
+   end
 end
 
 return Proofs
