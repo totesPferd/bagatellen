f1d0141e6f03a95e5a92e9e447ad9739c165b59c
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-11-22 09:49:42 +0100

    ...

--- lua/logics/male/rule/Assume.lua
+++ lua/logics/male/rule/Assume.lua
@@ -1,5 +1,4 @@
 local Resolve =  require "logics.male.rule.Resolve"
-local VarAssgnm =  require "logics.male.VarAssgnm"
 
 local Assume =  Resolve:__new()
 
@@ -9,8 +8,7 @@ local Set =  require "base.type.Set"
 
 function Assume:new(literal)
    local premises =  Set:empty_set_factory()
-   local var_assgnm =  VarAssgnm:new()
-   local clause =  Clause:new(premises, literal:devar(var_assgnm))
+   local clause =  Clause:new(premises, literal)
    return Resolve.new(self, clause)
 end
 
