460b9fb01e2ee46c39ffdd708b9857f126fbd9b9
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-11-25 08:12:59 +0100

    Revert "logics.pel.DefSymbol dropped (is not predicate!)"
    This reverts commit f1a317bf7ac81429c75c457eaa955e4b19a9e76c.

---
+++ lua/logics/pel/DefSymbol.lua
@@ -0,0 +1,40 @@
+local Type =  require "base.type.aux.Type"
+
+local DefSymbol =  Type:__new()
+
+package.loaded["logics.pel.DefSymbol"] =  DefSymbol
+
+function DefSymbol:new()
+   return self:__new()
+end
+
+function DefSymbol:get_def_symbol_cast()
+   return self
+end
+
+function DefSymbol:get_eq_symbol_cast()
+end
+
+function DefSymbol:get_qual_symbol_cast()
+end
+
+function DefSymbol:__eq(other)
+   local retval =  false
+   local other_def_symbol =  other:get_def_symbol()
+   if other_def_symbol
+   then
+      retval =  true
+   end
+   return retval
+end
+
+function DefSymbol:__diagnose_single_line(indentation)
+   indentation:insert(String:string_factory("(logics::pel::DefSymbol)"))
+end
+
+function DefSymbol:__diagnose_multiple_line(indentation)
+   indentation:insert(String:string_factory("(logics::pel::DefSymbol)"))
+end
+
+return DefSymbol
+
