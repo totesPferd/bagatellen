7efa69469766f5db9590b4245c526a948650c1eb
Georgi Rybakov <georgi.rybakov@gmx.net>
2017-11-25 09:22:56 +0100

    ...

---
+++ lua/logics/pel/rule/EqCong.lua
@@ -0,0 +1,54 @@
+local Resolve =  require "logics.male.rule.Resolve"
+
+local EqCong =  Resolve:__new()
+
+package.loaded["logics.pel.rule.EqCong"] =  EqCong
+local Clause =  require "logics.male.Clause"
+local Compound =  require "logics.pel.Compound"
+local EqSymbol =  require "logics.pel.EqSymbol"
+local List =  require "base.type.List"
+local ObjectVariable =  require "logics.pel.ObjectVariable"
+local Set =  require "base.type.Set"
+
+function EqCong:new(symbol, arity)
+   lhs_vars =  List:empty_list_factory()
+   rhs_vars =  List:empty_list_factory()
+   premises =  Set:empty_set_factory()
+   local eq_symbol =  EqSymbol:new()
+   for i in 1, arity
+   do local lhs_var =  ObjectVariable:new()
+      local rhs_var =  ObjectVariable:new()
+      local eq_args =  List:empty_list_factory()
+      eq_args:append(lhs_var)
+      eq_args:append(rhs_var)
+      lhs_vars:append(lhs_var)
+      rhs_vars:append(rhs_var)
+      premises:add(Compound:new(eq_symbol, eq_args))
+   end
+   local lhs_term =  Compound:new(symbol, lhs_vars)
+   local rhs_term =  Compound:new(symbol, rhs_vars)
+   local eq_args =  List:empty_list_factory()
+   eq_args:append(lhs_term)
+   eq_args:append(rhs_term)
+   local conclusion =  Compound:new(eq_symbol, eq_args)
+   local clause =  Clause:new(premises, conclusion)
+   return Resolve:new(clause)
+end
+
+function EqCong:get_eq_refl_cast()
+end
+
+function EqCong:get_eq_symm_cast()
+end
+
+function EqCong:get_eq_trans_cast()
+end
+
+function EqCong:get_eq_cong_cast()
+   return self
+end
+
+function EqCong:get_strict_cast()
+end
+
+return EqCong
---
+++ lua/logics/pel/rule/Strict.lua
@@ -0,0 +1,52 @@
+local Resolve =  require "logics.male.rule.Resolve"
+
+local Strict =  Resolve:__new()
+
+package.loaded["logics.pel.rule.Strict"] =  Strict
+local Clause =  require "logics.male.Clause"
+local Compound =  require "logics.pel.Compound"
+local DefSymbol =  require "logics.pel.DefSymbol"
+local List =  require "base.type.List"
+local MetaVariable =  require "logics.pel.ObjectVariable"
+local ObjectVariable =  require "logics.pel.ObjectVariable"
+local Set =  require "base.type.Set"
+
+function Strict:new(symbol, arity, place)
+   local conclusion_var =  ObjectVariable:new()
+   local args =  List:empty_list_factory()
+   for i = 1, arity
+   do if i == place
+      then
+         args:append(conclusion_var)
+      else
+         args:append(MetaVariable:new())
+      end
+   end
+   local premis_term =  Compound:new(symbol, args)
+   local def_symbol =  DefSymbol:new()
+   local premis =  Compound:new(def_symbol, premis_term)
+   local conclusion =  Compound:new(def_symbol, conclusion_var)
+   local premises =  Set:empty_set_factory()
+   premises:add(premis)
+
+   local clause =  Clause:new(premises, conclusion)
+   return Resolve:new(clause)
+end
+
+function Strict:get_eq_refl_cast()
+end
+
+function Strict:get_eq_symm_cast()
+end
+
+function Strict:get_eq_trans_cast()
+end
+
+function Strict:get_eq_cong_cast()
+end
+
+function Strict:get_strict_cast()
+   return self
+end
+
+return Strict
