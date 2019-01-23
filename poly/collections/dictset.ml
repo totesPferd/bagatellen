3d494e55c6e39dd5a79a39f890f1d0cb3110a162
Georgi Rybakov <georgi.rybakov@gmx.net>
2019-01-23 17:13:11 +0100

    turn to SML standard libraries

--- poly/collections/dictset.ml
+++ poly/collections/dictset.ml
@@ -1,19 +1,10 @@
-use "collections/list.ml";
-use "collections/option.ml";
-
 abstype ('a, 'b) dict =  dict of ('a * 'b) list
     and 'a set =  set of 'a list
 with
     val empty_d =  dict nil
     fun map_d f (dict kl) =  dict (map (fn (k, v) => (k, f(v))) kl)
     fun keys (set kd) =  set (map (fn (k, v) => k) kd)
-    fun deref(k, dict nil) =  none
-      | deref(k, dict ((f, v) :: ll))
-      = if  k = f
-        then
-           some v
-        else
-           deref(k, dict ll)
+    fun deref(k, dict ll) =  Option.map (fn (f, v) => v) (List.find (fn (f, v) => (k = f)) ll)
     local
        fun lset(k, v, nil) =  [(k, v)]
          | lset(k, v, ((f, w) :: ll))
@@ -26,7 +17,7 @@ with
        fun set_d (k, v, dict ll) =  dict(lset(k, v, ll))
     end
 
-    fun deref_r(k, ref_d) =  map_o ! (deref(k, !ref_d))
+    fun deref_r(k, ref_d) =  Option.map ! (deref(k, !ref_d))
     local
        fun lset_r(k, v, nil)
          = let
@@ -49,13 +40,19 @@ with
 
     val empty_s =  set nil
     fun singleton_s(x) =  set [ x ]
-    fun is_member_s(x, set ll) =  is_member_l(x, ll)
-    fun drop_s(x, set ll) =  set(drop_l(x, ll))
-    fun insert_s(x, set ll) =  set(insert_l(x, ll))
+    fun is_member_s(x, set ll) =  List.exists (fn (y) => x = y) ll
+    fun drop_s(x, set ll) =  set(List.filter (fn (y) => not(x = y)) ll)
+    fun insert_s(x, set ll)
+      = set(
+           if is_member_s(x, set ll)
+           then
+              ll
+           else
+              x :: ll )
     fun union(set nil, ls) =  ls
       | union(set (x :: kl), ls) =  insert_s (x, union(set kl, ls))
     fun cut(ks, set nil) =  ks
       | cut(ks, set(y :: ll)) =  cut(drop_s(y, ks), set ll)
-    fun subseteq_s(set kl, set ll) =  subseteq_l(kl, ll)
+    fun subseteq_s(ks, ls) =  List.all (fn (x) => is_member_s(x, ls)) ks
     fun map_s f (set kl) =  set (map f kl)
 end;
--- poly/collections/option.ml
+++
@@ -1,5 +0,0 @@
-datatype 'a option =  none | some of 'a;
-
-fun map_o f none =  none
-  | map_o f (some x) =  some (f x);
-
