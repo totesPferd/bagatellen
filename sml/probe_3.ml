656305fd7512209209bf3d2b1e57babb2778a02d
Joerg Fischer <georgi.rybakov@gmx.net>
2020-10-03 16:06:14 +0200

    proceed probe_3.ml

--- sml/probe_3.ml
+++ sml/probe_3.ml
@@ -31,4 +31,52 @@ structure MyPELLiteralsMap: BaseMap =
       fun get_map f =  f
    end;
 
+structure MyPELLiteralsPointeredTypeGenerating =  NamingPointeredTypeGenerating (
+   struct
+      structure BaseType =  MyPELLiteralsConstruction.Variables.Base.Single;
+      structure BaseStructureMap =  MyPELLiteralsMap;
+      structure BinaryRelation =  MyPELLiteralsBinaryRelation;
+      structure PolymorphicContainerType =  NamingPolymorphicContainerType;
+   end );
+
+structure MyPELVariablesDictSet =  DictSet(MyPELLiteralsConstruction.Variables)
+structure MyPELOccurences =  Occurences(MyPELVariablesDictSet)
+
+structure MyPELDictMap =  DictMap(
+   struct
+      structure DS =  MyPELVariablesDictSet
+      structure End =  MyPELLiteralsConstruction.Variables
+   end );
+
+structure MyPELVariablesBinaryRelation: BaseBinaryRelation =
+   struct
+      structure Domain =  MyPELLiteralsConstruction.Variables
+      structure Relation =
+         struct
+            type T =  Domain.T * Domain.T -> bool
+         end
+      fun apply f (x, y) = f (x, y)
+      fun get_binary_relation f =  f
+   end;
+
+structure MyPELVariablesMap: BaseMap =
+   struct
+      structure Start =  MyPELLiteralsConstruction.Variables
+      structure End =  MyPELLiteralsConstruction.Variables
+      structure Map =
+         struct
+            type T =  Start.T -> End.T
+         end
+
+      fun apply f x =  f x
+      fun get_map f =  f
+   end;
+
+structure MyPELVariablesStructure =  VariableAsStructure(
+   struct
+      structure Variables =  MyPELLiteralsConstruction.Variables
+      structure BinaryRelation =  MyPELVariablesBinaryRelation
+      structure Map =  MyPELVariablesMap
+   end );
+
 
