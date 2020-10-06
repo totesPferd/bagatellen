c81c0f74d5bfff1aae1d76e1a82d8b312ee8e0fc
Joerg Fischer <georgi.rybakov@gmx.net>
2020-10-06 16:09:55 +0200

    ...

--- sml/probe_5.ml
+++ sml/probe_5.ml
@@ -7,3 +7,19 @@ structure MyQualifiedLiteralsConstruction =  QualifiedLiteralsConstruction (
       structure PCT =  NamingPolymorphicContainerType;
       structure PV =  PolymorphicVariables;
    end );
+
+structure MyQualifiedBaseType: SumType =  SumType (
+   struct
+      structure FstType =  MyQLLiteralsConstruction.Variables;
+      structure SndType =  MyPELLiteralsConstruction.Variables;  (* ??? *)
+   end );
+
+structure MyQualifiedIncludeDictSet =  DictSet(MyQualifiedBaseType)
+structure MyQualifiedIncludeOccurences =  Occurences(MyQualifiedIncludeDictSet)
+
+structure MyQualifiedOccurences =  QualifiedOccurences (
+   struct
+      structure Include =  MyQualifiedIncludeOccurences;
+      structure Qualifier =  MyQLOccurences;
+      structure QualifiedBaseType =  MyQualifiedBaseType;
+   end );
