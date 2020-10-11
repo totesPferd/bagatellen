69ddf07f0a149e3dfee54e59a37fe61038c6bd54
Joerg Fischer <georgi.rybakov@gmx.net>
2020-10-11 14:15:52 +0200

    ...

--- python/dctbnbc/dctbnbc/evaluate.py
+++ python/dctbnbc/dctbnbc/evaluate.py
@@ -1,7 +1,4 @@
-import dctbnbc.tokenize
-
-
-def refused_to_be_a_bad_guy(knowledge, content):
+def refused_to_be_a_bad_guy(knowledge, token_list):
    retval =  False
    for word in dctbnbc.tokenize.tokenize(content):
       if word in knowledge["used_by_good_guys_only"]:
@@ -11,7 +8,7 @@ def refused_to_be_a_bad_guy(knowledge, content):
    return retval
 
 
-def refused_to_be_a_good_guy(knowledge, content):
+def refused_to_be_a_good_guy(knowledge, token_list):
    retval =  False
    for word in dctbnbc.tokenize.tokenize(content):
       if word in knowledge["used_by_bad_guys_only"]:
@@ -21,11 +18,10 @@ def refused_to_be_a_good_guy(knowledge, content):
    return retval
 
 
-def get_score(knowledge, content):
+def get_score(knowledge, token_list):
    retval =  0
 
-   token_list =  dctbnbc.tokenize.tokenize(content)
-   size =  len(content)
+   size =  sum ([ len(token) for token in token_list ])
    for word in knowledge["score"].keys():
      if word not in token_list:
         retval =  retval - knowledge["score"][word] * size / len(content)
