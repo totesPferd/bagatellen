0bf26be6c26984328648ac4b788abe1bcee6987c
Joerg Fischer <georgi.rybakov@gmx.net>
2020-10-11 10:06:17 +0200

    + dctbnbc.extract_authors_from_bad_sites

--- python/dctbnbc/dctbnbc/config_file.py
+++ python/dctbnbc/dctbnbc/config_file.py
@@ -1,10 +1,23 @@
 import json
 import os
 import os.path
+import sys
 
 def get_config_from_file(result):
+   retval =  False
+
    file_path =  os.path.join(os.getenv("HOME"), "dctbnbc.json")
-   with open(file_path, "r") as fd:
-      raw_data =  fd.read()
-      result =  json.loads(raw_data)
-      return "bad_guys" in result.keys() and "bad_sites" in result.keys() and "good_sites" in result.keys()
+   try:
+      with open(file_path, "r") as fd:
+         raw_data =  fd.read()
+         json_data =  json.loads(raw_data)
+         result["bad_guys"] =  json_data["bad_guys"]
+         result["bad_sites"] =  json_data["bad_sites"]
+         result["good_sites"] =  json_data["good_sites"]
+         retval =  True
+   except FileNotFoundError:
+      sys.stderr.write("config file %s not found.\n" % file_path)
+   except json.decoder.JSONDecodeError:
+      sys.stderr.write("no valid json data in config file %s.\n" % file_path)
+
+   return retval
