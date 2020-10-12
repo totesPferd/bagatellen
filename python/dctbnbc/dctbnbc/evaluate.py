cbe875f165f696b3c138309a12275ca6221567d0
Joerg Fischer <georgi.rybakov@gmx.net>
2020-10-12 18:49:01 +0200

    Revert "..."
    This reverts commit 810933ed6a2566d4382245154fc289a298fbc7c1.

--- python/dctbnbc/dctbnbc/evaluate.py
+++ python/dctbnbc/dctbnbc/evaluate.py
@@ -3,7 +3,7 @@ def get_score(knowledge, token_list):
 
    size =  len(token_list)
    for word in knowledge["scores"].keys():
-     nr_of_occurences =  len ([ w for w in token_list if word == w ])
-     retval =  retval - nr_of_occurences * knowledge["scores"][word] / size
+     if word not in token_list:
+        retval =  retval - knowledge["scores"][word] * size
 
    return retval
