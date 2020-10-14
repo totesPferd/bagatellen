13c380e66a3acdaedcf4eb4274f02ee4bf8ab753
Georgi Rybakov <georgi.rybakov@gmx.net>
2020-10-14 04:34:16 +0200

    + doit.sh

---
+++ python/dctbnbc/doit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd `dirname "$0"`
+
+cat bad.json | python3 -m dctbnbc.get_init_token_list -u -f bad_sites.json >bad.json.bak
+python3 -m dctbnbc.update_token_list <bad.json.bak >bad.json || mv bad.json.bak bad.json
+cat good.json | python3 -m dctbnbc.get_init_token_list -u -f good_sites.json >good.json.bak
+python3 -m dctbnbc.update_token_list <good.json.bak >good.json || mv good.json.bak good.json
+
