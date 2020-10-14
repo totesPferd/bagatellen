85a005e2d0c0c6749494bb3035dbb5b6a5d8b9ef
Joerg Fischer <georgi.rybakov@gmx.net>
2020-10-14 04:21:23 +0200

    dctbnbc.get_init_token_list

--- python/dctbnbc/dctbnbc/get_init_token_list.py
+++ python/dctbnbc/dctbnbc/get_init_token_list.py
@@ -9,8 +9,9 @@ def print_usage(file):
    file.write("\nUSAGE:\n\n")
    file.write("%s [-h|--help]\n" % sys.argv[0])
    file.write("   write this usage information.\n\n")
-   file.write("%s [{-f|--file} <site_files] ... [<feed_url>] ...\n" % sys.argv[0])
+   file.write("%s [-u|--update] [{-f|--file} <site_files] ... [<feed_url>] ...\n" % sys.argv[0])
    file.write("   provide initial data to <stdout> which can be consumed by dctbnbc.update_token_list program.\n")
+   file.write("   give -u switch for update mode: read from <stdin>.\n")
 
 
 def interpret_cmdline(result):
@@ -18,7 +19,10 @@ def interpret_cmdline(result):
    retval =  "OK"
 
    try:
-      optlist, args =  getopt.getopt(sys.argv[1:], "hf:", [ "help", "file" ])
+      optlist, args =  getopt.getopt(sys.argv[1:], "hf:u", [ "help", "file", "update" ])
+
+      if "update_mode" not in result.keys():
+         result["update_mode"] =  False
 
       if "files" not in result.keys():
          result["files"] =  []
@@ -33,6 +37,12 @@ def interpret_cmdline(result):
                retval =  "HelpMode"
          elif option in { "-f", "--file" }:
             result["files"].append(arg)
+         elif option in { "-u", "--update" }:
+            if result["update_mode"]:
+               sys.stderr.write("give -u option once only!\n");
+               retval =  "Error"
+            else:
+               result["update_mode"] =  True
          else:
             sys.stderr.write("option %s not permitted.\n\n" % option)
             retval =  "Error"
@@ -49,6 +59,22 @@ def interpret_cmdline(result):
 
    return retval
 
+
+def add_post(out_data, url):
+   is_found =  False
+   for eurl in out_data["posts"]:
+      if "href" in eurl and isinstance(eurl["href"], str):
+         if eurl["href"] == url:
+            is_found = True
+            break
+      else:
+         sys.stderr.write("there are malformed posts in post region in json file.\n")
+   if not is_found:
+      out_data["posts"].append({
+            "href": url
+         ,  "ids": [] })
+         
+
 # interprete cmdline.
 cmdline_params =  {}
 retval =  interpret_cmdline(cmdline_params)
@@ -60,9 +86,34 @@ elif retval == "HelpMode":
 out_data =  {
       "nr": 0
    ,  "abundance": {}
-   ,  "posts": [{
-            "href": url
-         ,  "ids": [] } for url in cmdline_params["urls"] ]}
+   ,  "posts": [] }
+if cmdline_params["update_mode"]:
+   raw_stdin_data =  sys.stdin.read()
+   try:
+      out_data =  json.loads(raw_stdin_data)
+   except json.decoder.JSONDecodeError:
+      sys.stderr.write("<stdin> not a json file.\n")
+      sys.exit(2)
+
+   errval =  0
+
+   if "nr" not in out_data or not isinstance(out_data["nr"], int):
+      sys.stderr.write("<stdin> json file does not contain nr key assigning to int.\n")
+      errval =  2
+
+   if "abundance" not in out_data or not isinstance(out_data["abundance"], dict):
+      sys.stderr.write("<stdin> json file does not contain abundance key assigning to dict.\n")
+      errval =  2
+
+   if "posts" not in out_data or not isinstance(out_data["posts"], list):
+      sys.stderr.write("<stdin> json file does not contain posts key assigning to dict.\n")
+      errval =  2
+
+   if errval != 0:
+      sys.exit(2)
+
+for url in cmdline_params["urls"]:
+   add_post(out_data, url)
 
 for f in cmdline_params["files"]:
    try:
@@ -72,7 +123,7 @@ for f in cmdline_params["files"]:
          if "sites" in json_f_data and isinstance(json_f_data["sites"], list):
             for site in json_f_data["sites"]:
                if "href" in site:
-                  out_data["posts"].append({ "href": site["href"], "ids": [] })
+                  add_post(out_data, site["href"])
                else:
                   sys.stderr.write("href key is missing in a element of list assigned to sites key in json file %s given in -f cmdline param.\n" % f)
          else:
@@ -80,6 +131,7 @@ for f in cmdline_params["files"]:
          if "authors" in json_f_data and isinstance(json_f_data["authors"], list):
             if "authors" in out_data:
                out_data["authors"] =  list(set(out_data["authors"]) | set(json_f_data["authors"]))
+               out_data["authors"].sort()
             else:
                out_data["authors"] =  json_f_data["authors"]
    except FileNotFoundError:
