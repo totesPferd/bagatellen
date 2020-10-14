e9c063206c3e21e9c8c2d3390adfeb11a85adc83
Georgi Rybakov <georgi.rybakov@gmx.net>
2020-10-14 09:21:51 +0200

    ...

--- python/dctbnbc/doit.sh
+++ python/dctbnbc/doit.sh
@@ -4,6 +4,8 @@ cd `dirname "$0"`
 
 cat bad.json | python3 -m dctbnbc.get_init_token_list -u -f bad_sites.json >bad.json.bak
 python3 -m dctbnbc.update_token_list <bad.json.bak >bad.json || mv bad.json.bak bad.json
+cat bad.json | python3 -m dctbnbc.get_token_list >bad_tl.json
 cat good.json | python3 -m dctbnbc.get_init_token_list -u -f good_sites.json >good.json.bak
 python3 -m dctbnbc.update_token_list <good.json.bak >good.json || mv good.json.bak good.json
+cat good.json | python3 -m dctbnbc.get_token_list >good_tl.json
 
