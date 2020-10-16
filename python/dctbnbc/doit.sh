ec20ff185aa1c91e535191a92dc351767867cece
Joerg Fischer <georgi.rybakov@gmx.net>
2020-10-16 16:15:27 +0200

    ...

--- python/dctbnbc/doit.sh
+++ python/dctbnbc/doit.sh
@@ -8,4 +8,4 @@ cat bad.json | python3 -m dctbnbc.get_token_list >bad_tl.json
 cat good.json | python3 -m dctbnbc.get_init_token_list -u -f good_sites.json >good.json.bak
 python3 -m dctbnbc.update_token_list <good.json.bak >good.json || mv good.json.bak good.json
 cat good.json | python3 -m dctbnbc.get_token_list >good_tl.json
-python3 -m dctbnbc.subtract -a bad_tl.json -b good_tl.json >knowledge
+python3 -m dctbnbc.subtract -a good_tl.json -b bad_tl.json >knowledge
