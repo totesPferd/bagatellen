75ac45f8c9363cd887fd58534fa7665c2fd473a9
Joerg Fischer <georgi.rybakov@gmx.net>
2020-10-19 15:19:58 +0200

    + setup.py

---
+++ python/dctbnbc/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import setuptools
+
+
+setuptools.setup(name = "dctbnbc", version = "0.1",
+ packages=setuptools.find_packages(),
+ author = "Georgi Rybakov",
+ author_email = "georgi.rybakov@oos.cc",
+ url    = "http://georgi.rybakov.oos.cc"
+)
