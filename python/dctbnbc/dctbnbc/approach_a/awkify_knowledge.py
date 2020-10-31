14b51fcf4f34d5a9c40533c851c9f5eab5dc6884
Joerg Fischer <georgi.rybakov@gmx.net>
2020-10-31 16:28:00 +0100

    + dctbnbc.approach_a.awkify_knowledge

---
+++ python/dctbnbc/dctbnbc/approach_a/awkify_knowledge.py
@@ -0,0 +1,68 @@
+import getopt
+import json
+import sys
+
+def print_usage(file):
+   file.write("\nUSAGE:\n\n")
+   file.write("%s [-h|--help]\n" % sys.argv[0])
+   file.write("   write this usage information.\n\n")
+   file.write("%s\n" % sys.argv[0])
+   file.write("   print out a awkable list of scores onto <stdout> sorted by score.\n")
+
+
+def interpret_cmdline(result):
+
+   retval =  "OK"
+
+   try:
+      optlist, args =  getopt.getopt(sys.argv[1:], "h", [ "help" ])
+
+      for option, arg in optlist:
+         if option in { "-h", "--help" }:
+            if retval == "OK":
+               print_usage(sys.stdout)
+               retval =  "HelpMode"
+         else:
+            sys.stderr.write("option %s not permitted.\n\n" % option)
+            retval =  "Error"
+
+      if len(args) != 0:
+         sys.stderr.write("do not give cmdline params\n")
+         retval =  "Error"
+
+   except getopt.GetoptError as err:
+      sys.stderr.write("%s\n\n" % str(err))
+      retval =  "Error"
+
+   if retval == "Error":
+      print_usage(sys.stderr)
+
+   return retval
+
+
+# interprete cmdline.
+cmdline_params =  {}
+retval =  interpret_cmdline(cmdline_params)
+if retval == "Error":
+   sys.exit(2)
+elif retval == "HelpMode":
+   sys.exit(0)
+
+raw_stdin_data =  sys.stdin.read()
+try:
+   json_stdin_data =  json.loads(raw_stdin_data)
+except json.decoder.JSONDecodeError:
+   sys.stderr.write("<stdin> not a json file.\n")
+   sys.exit(2)
+
+errval =  0
+if "scores" not in json_stdin_data or not isinstance(json_stdin_data["scores"], dict):
+   sys.stderr.write("scores key assigning to a dict in <stdin> json file is missing.\n")
+
+if errval != 0:
+   sys.exit(errval)
+
+listified_scores =  [ (json_stdin_data["scores"][k], k) for k in json_stdin_data["scores"] ]
+listified_scores.sort()
+for (v, k) in listified_scores:
+   print(k, v)
