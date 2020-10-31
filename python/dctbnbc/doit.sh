5641de2de93790525efeed1afeaf37acd75f15de
Joerg Fischer <georgi.rybakov@gmx.net>
2020-10-31 16:10:22 +0100

    mehr Verfahren fuer Auswertung vorgesehen

--- python/dctbnbc/doit.sh
+++ python/dctbnbc/doit.sh
@@ -10,4 +10,4 @@ cp good.json good.json.bak
 python3 -m dctbnbc.get_init_token_list -u -f good_sites.json -s good.json
 python3 -m dctbnbc.update_token_list -s good.json
 cat good.json | python3 -m dctbnbc.get_token_list >good_tl.json
-python3 -m dctbnbc.gen_knowledge -a good_tl.json -b bad_tl.json >knowledge
+python3 -m dctbnbc.approach_b.gen_knowledge -a good_tl.json -b bad_tl.json >knowledge
